961b67f4c41e3e86315a7d8b52a432f7fd64c75a
Hazem <hazem_tripleh@hotmail.com>
2026-04-10 15:08:27 -0400

    removed dockerfiles

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python3 manage.py collectstatic --noinput
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-gunicorn --bind 0.0.0.0:8081 --workers 3 service.wsgi:application --access-logfile - --error-logfile -
